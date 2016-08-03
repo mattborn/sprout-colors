40566a839f07895fa02c1f61424e86dc9f957a47
Matt Born <mattborn@users.noreply.github.com>
2016-08-03 10:48:05 -0500

    Remove duplicates + format network colors

--- Sprout Colors.sketchplugin/Contents/Sketch/SproutColors.js
+++ Sprout Colors.sketchplugin/Contents/Sketch/SproutColors.js
@@ -1,31 +1,53 @@
 var app = NSApp.delegate();
 
+var formatted = [];
+var hex = [];
+var hex_dedupe = [];
+
+function convertToHex(colors) {
+  colors.forEach(function(color) {
+    var c = MSColor.colorWithRed_green_blue_alpha(
+      color.red(),
+      color.green(),
+      color.blue(),
+      color.alpha()
+    );
+    hex.push(c.hexValue());
+  });
+}
+
+// inspired by _.uniq
+function dedupe(array, result) {
+  array.forEach(function (item) {
+    if (result.indexOf(item) < 0) {
+      result.push(item);
+    }
+  });
+}
+
 function formatDocumentColors(context) {
   var doc = context.document;
   var colors = doc.documentData().assets().colors();
-  var formatted = [];
 
   try {
     if (colors.length === 111) {
-      colors.forEach(function(color, index) {
-        // for later:
-        // var c = MSColor.colorWithRed_green_blue_alpha(
-        //   color.red(),
-        //   color.green(),
-        //   color.blue(),
-        //   color.alpha()
-        // );
-        // hexValues.push(c.hexValue());
-        formatted.push(color);
-        if ((index + 1) % 6 === 0) {
+      convertToHex(colors);
+      dedupe(hex, hex_dedupe);
+      hex_dedupe.forEach(function (color, index) {
+        var i = index + 1;
+        formatted.push(MSColor.colorWithSVGString('#'+ color));
+        if (i < 73 && i % 6 === 0 || i > 72 && i % 4 === 0) {
           formatted.push(MSColor.colorWithSVGString('#FFFFFF'));
+          formatted.push(MSColor.colorWithSVGString('#FFFFFF'));
+        }
+        if (i > 72 && i % 2 === 0) {
           formatted.push(MSColor.colorWithSVGString('#FFFFFF'));
         }
       });
       doc.documentData().assets().setColors(formatted);
       NSApp.displayDialog('Document colors are now formatted.');
     } else {
-      NSApp.displayDialog('Add Styles and Symbols to Document in Brand.ai plugin');
+      NSApp.displayDialog('Clear Document colors, then Add Styles and Symbols to Document in Brand.ai plugin');
     }
   } catch(e) {
     log(e);
